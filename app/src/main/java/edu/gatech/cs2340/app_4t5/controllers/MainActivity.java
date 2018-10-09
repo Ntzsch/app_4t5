8dae5a1e4c2fb569cf0554251813121bf07104c1
Reece Gao <rgao@gatech.edu>
2018-10-09 00:39:40 -0400

    navigation bar implemented

--- app/src/main/java/edu/gatech/cs2340/app_4t5/controllers/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/app_4t5/controllers/MainActivity.java
@@ -2,32 +2,55 @@ package edu.gatech.cs2340.app_4t5.controllers;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.support.v7.widget.Toolbar;
 
 import edu.gatech.cs2340.app_4t5.R;
 import edu.gatech.cs2340.app_4t5.models.User;
 import edu.gatech.cs2340.app_4t5.models.UserRecords;
 
 public class MainActivity extends AppCompatActivity{
-    private Button logout_button;
+    //private Button logout_button;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.location_listview);
 
-        logout_button = findViewById(R.id.logout_button);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
 
-        logout_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
-                finish();
-                startActivity(i);
-            }
-        });
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
 
+//        logout_button = findViewById(R.id.logout_button);
+//
+//        logout_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+//                finish();
+//                startActivity(i);
+//            }
+//        });
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
